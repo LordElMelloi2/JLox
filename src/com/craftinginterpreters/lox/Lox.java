5eaaca7a0bf44deb2b7eb3018852d83d06f3ead2
LordElMelloi2 <ibdaxter@hotmail.com>
2023-04-19 16:25:17 -0600

    End of chapter 7, Crafting Interpreters

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,13 +10,16 @@ import java.util.List;
 
 public class Lox {
     
+    private static final Interpreter interpreter = new Interpreter();    
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     
 
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if(hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
     
     private static void runPrompt() throws IOException {
@@ -42,11 +45,17 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
     
     static void error(int line, String message) {
         report(line, "", message);
+    }
+    
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+          "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
