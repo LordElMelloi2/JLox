51fd4468902477b0715643dd41120208dd0fd2c8
LordElMelloi2 <ibdaxter@hotmail.com>
2023-04-25 18:29:57 -0600

    Complete chapter 10, Crafting Interpreters

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,12 +1,76 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     
     private static class BreakException extends RuntimeException {}
     
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    public Interpreter() {
+        globals.define("clock", new LoxCallable() {
+        
+            @Override
+            public int arity() { return 0; }
+
+            @Override
+            public Object call(Interpreter interpreter,
+                             List<Object> arguments) {
+            return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+        
+        globals.define("isan", new LoxCallable(){
+            @Override
+            public int arity() { return 1; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                try{
+                    Double d = (Double)arguments.get(0);
+                    return true;
+                }catch(Exception e){
+                    return false;
+                }
+            }
+            
+            @Override
+            public String toString() { return "<native fn>"; }
+            
+        });
+        
+        globals.define("stringify", new LoxCallable() {
+            @Override
+            public int arity() {
+                return 1;
+            }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                if (arguments.get(0) == null) return "nil";
+
+                if (arguments.get(0) instanceof Double) {
+                    String text = arguments.get(0).toString();
+
+                    if (text.endsWith(".0")) {
+                        text = text.substring(0, text.length() - 2);
+                    }
+                    return text;
+                }
+
+                return arguments.get(0).toString();
+            }
+            
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+    }
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -103,6 +167,31 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
     
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) { 
+            arguments.add(evaluate(argument));
+        }
+        
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+              "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+        
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " +
+              function.arity() + " arguments but got " +
+              arguments.size() + ".");
+        }
+        
+        return function.call(this, arguments);
+    }
+    
     private void checkNumberOperand(Token operator, Object operand) {
         if (operand instanceof Double) return;
         throw new RuntimeError(operator, "Operand must be a number.");
@@ -153,6 +242,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
     
     @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        LoxFunction function = new LoxFunction(stmt, environment);
+        environment.define(stmt.name.lexeme, function);
+        return null;
+    }
+    
+    @Override
     public Void visitIfStmt(Stmt.If stmt) {
     if (isTruthy(evaluate(stmt.condition))) {
         execute(stmt.thenBranch);
@@ -167,6 +263,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
+    }
+    
+    @Override
+    public Void visitReturnStmt(Stmt.Return stmt) {
+        Object value = null;
+        if (stmt.value != null) value = evaluate(stmt.value);
+
+        throw new Return(value);
     }
     
     @Override
